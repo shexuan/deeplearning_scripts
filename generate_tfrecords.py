816ab143af038c77f3648c7bd343ebdd181d0f37
shexuan <shexuan523392@gmail.com>
2019-05-06 10:15:35 +0800

    debugs

--- generate_tfrecords.py
+++ generate_tfrecords.py
@@ -4,9 +4,11 @@
 import tensorflow as tf
 import numpy as np
 import os
+import math
 import random
 import multiprocessing as mp
 from PIL import Image
+from collections import defaultdict
 
 
 def _int64_feature(value):
@@ -32,7 +34,7 @@ def _float_feature(value):
 def _make_example(filename, label):
     name = filename.split('/')[-1]
     img_data = Image.open(filename)
-    img_data = img_data.tostring()
+    img_data = img_data.tobytes()
     example = tf.train.Example(
         features=(tf.train.Features(
             feature={
@@ -52,30 +54,42 @@ def write_tfrecords(output, files):
 
 
 def main():
-    total_classes = [os.path.join(FLAGS.data_dir, d) for d in os.listdir(FLAGS.data_dir) if os.path.isdir(d)]
-    # total_files ==> [(abs_file1 , class_1),(abs_file2, class_2),...]
-    total_files = [(os.path.join(d, f), label) for d, label in enumerate(total_classes) for f in os.listdir(d)]
+    print(os.listdir(FLAGS.data_dir))
+    total_classes = [os.path.join(FLAGS.data_dir, d) for d in os.listdir(FLAGS.data_dir)
+                     if os.path.isdir(os.path.join(FLAGS.data_dir, d))]
+    print(total_classes)
+    train_data = []
+    eval_data = []
+    for label, dir_ in enumerate(total_classes):
+        data = [(os.path.join(dir_, f), label) for f in os.listdir(dir_)]
+        if FLAGS.train_eval:
+            eval_num = FLAGS.eval_percent * len(data)
+            eval_data.extend(data[:int(eval_num)])
+            train_data.extend(data[int(eval_num):])
+    train_num = len(train_data)
+    eval_num = len(eval_data)
+    print(train_num, FLAGS.train_tfrecords_num)
+    train_num_per_tfrecord = math.ceil(train_num / FLAGS.train_tfrecords_num)
+    eval_num_per_tfrecord = math.ceil(eval_num / FLAGS.eval_tfrecords_num)
+
     random.seed(1234)
-    random.shuffle(total_files)
-    if FLAGS.train_eval:
-        eval_num = FLAGS.eval_percent*len(total_files)
-        train_num = len(total_files)-int(eval_num)
-        eval_data = total_files[:int(eval_num)]
-        train_data = total_files[int(eval_data):]
-        train_num_per_tfrecord = train_num//FLAGS.train_tfrecords_num
-        eval_num_per_tfrecord = eval_num//FLAGS.eval_tfrecords_num
-    else:
-        train_data = total_files
-        train_num_per_tfrecord = train_data//FLAGS.train_tfrecords_num
+    random.shuffle(train_data)
+    random.seed(1234)
+    random.shuffle(eval_data)
+
     # 多进程写入tfrecords文件
     writer_args = []
+    n = 1
     for i in range(0, len(train_data), train_num_per_tfrecord):
-        tfrec_name = 'train_'+str(i)+'_of_'+str(FLAGS.train_tfrecords_num)
-        writer_args.append([tfrec_name, train_data[i:i+train_num_per_tfrecord]])
+        tfrec_name = FLAGS.outdir + '/train_' + str(n) + '_of_' + str(FLAGS.train_tfrecords_num)
+        writer_args.append([tfrec_name, train_data[i:i + train_num_per_tfrecord]])
+        n += 1
     if FLAGS.train_eval:
+        n = 1
         for i in range(0, len(eval_data), eval_num_per_tfrecord):
-            tfrec_name = 'eval_'+str(i)+'_of_'+str(FLAGS.eval_tfrecords_num)
-            writer_args.append([tfrec_name, eval_data[i:i+eval_num_per_tfrecord]])
+            tfrec_name = FLAGS.outdir + '/eval_' + str(n) + '_of_' + str(FLAGS.eval_tfrecords_num)
+            writer_args.append([tfrec_name, eval_data[i:i + eval_num_per_tfrecord]])
+            n += 1
     pool = mp.Pool(FLAGS.num_processes)
     pool.starmap(write_tfrecords, writer_args)
 
@@ -84,11 +98,14 @@ if __name__ == '__main__':
     flags = tf.app.flags
     flags.DEFINE_string('data_dir', '', """Directory of data to be converted to tfrecords."""
                         """NOTE that each class should be stored in a child directory.""")
-    flags.DEFINE_string('num_processes', '10', """Number of processes to process data.""")
+    flags.DEFINE_integer('num_processes', 10, """Number of processes to process data.""")
     flags.DEFINE_boolean('train_eval', True, """Dataset split for train and evaluation.""")
     flags.DEFINE_float('eval_percent', 0.2, """Evaluation data ration in total dataset.""")
     flags.DEFINE_integer('train_tfrecords_num', 10, """Number of tfrecords to store train dataset.""")
     flags.DEFINE_integer('eval_tfrecords_num', 1, """Number of tfrecords to store evaluation dataset.""")
     flags.DEFINE_string('outdir', '', """Output directory for tfrecords""")
+
+    global FLAGS
     FLAGS = flags.FLAGS
+
     main()
